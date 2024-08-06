2f7ca53b7da7a4e6ed7d518a11b4eb5ce9340f42
Akshay <8009akshy@gmail.com>
2024-08-06 15:54:16 +0530

    test101

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 
 import React from "react";
 import './../styles/App.css';
-import UserInfo from "./UserInfo"
+import { useDispatch, useSelector } from "react-redux";
+import{updateName,updateEmail} from "./action"
 
 const App = () => {
+const dispatch=useDispatch();
+const {name,email}=useSelector(state=>state);
+  const handleName=(e)=>{
+     dispatch(updateName(e.target.value));
+  }
+
+  const handleEmail=(e)=>{
+    dispatch(updateEmail(e.target.value));
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <UserInfo/>
+        <h1>User Information</h1>
+        <form>
+          <label>Name:
+            <input type="text" value={name} onChange={handleName}/>
+          </label>
+          <br></br>
+          <label>Email:
+            <input type="email" value={email} onChange={handleEmail}/>
+          </label>
+        </form>
+        <h3>Current values in store:</h3>
+        <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+        </div>
     </div>
   )
 }
